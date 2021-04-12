9fb035cd9ebe1626c8555c6c530d6f25703695ae
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2021-04-12 14:50:32 +0200

    feat: guest counter add

--- dine/components/booking/BookingForm/GuestCounter/index.js
+++ dine/components/booking/BookingForm/GuestCounter/index.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import { Counter, CounterButton } from './styles';
 
 const GuestCounter = () => {
+  const [count, setCount] = useState(4);
+
   return (
     <Counter>
-      <CounterButton>
+      <CounterButton onClick={() => setCount(count - 1)}>
         <img src="/icons/icon-minus.svg" alt="" role="presentation" />
       </CounterButton>
-      <span>4 people</span>
-      <CounterButton>
+      <span>{count > 1 ? `${count} people` : '1 person'}</span>
+      <CounterButton onClick={() => setCount(count + 1)}>
         <img src="/icons/icon-plus.svg" alt="" role="presentation" />
       </CounterButton>
     </Counter>
